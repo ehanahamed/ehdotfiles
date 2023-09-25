c36db255a14eb774b472d6e45bd7d018a04d24ab
Ehan Ahamed <ehanahamed@ehan.dev>
2023-09-25 14:47:04 -0400

    fzf again?

---
+++ src/ehansCustomStuff/sh/purpleishFzf.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#a3a3cc,bg:#1e1e32,border:#3d3d66,hl:#3D997A,fg+:#a3a3cc,bg+:#1e1e32,hl+:#52cca3,info:#5c5c99,prompt:#7a52cc,pointer:#7a52cc,marker:#5c5c99,spinner:#7a52cc,header:#7a52cc'
