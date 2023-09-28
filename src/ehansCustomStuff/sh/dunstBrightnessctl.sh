75f39c133f808c5fb4b6a6b945a5475811dbf9cc
Ehan Ahamed <ehanahamed@ehan.dev>
2023-09-27 21:12:04 -0400

    Update custom notification stuff

--- src/ehansCustomStuff/sh/dunstBrightnessctl.sh
+++ src/ehansCustomStuff/sh/dunstBrightnessctl.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-notify-send -u low -t 600 -h int:value:$(brightnessctl --device=intel_backlight get -P) "Brightness: $(brightnessctl --device=intel_backlight get -P)%"
+# First clear old notifications
+dunstctl close-all
+
+notify-send -u low -t 1000 -h int:value:$(brightnessctl --device=intel_backlight get -P) "Brightness: $(brightnessctl --device=intel_backlight get -P)%"
--- src/ehansCustomStuff/sh/dunstPulsemixer.sh
+++ src/ehansCustomStuff/sh/dunstPulsemixer.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+# First clear old notifications
+dunstctl close-all
 # Will get the current speaker's volume, no extra setup needed for headphones, bluetooth speakers, etc
 volumeraw=$(pulsemixer --get-volume)
 volume=${volumeraw%% *}
 if [ $(pulsemixer --get-mute) == 1 ]; then
-  notify-send -u low -t 600 -h int:value:$volume "Volume: $volume%" "(muted)"
+  notify-send -u low -t 1000 -h int:value:$volume "Volume: $volume%" "(muted)"
 else
-  notify-send -u low -t 600 -h int:value:$volume "Volume: $volume%" "(UNMUTED)"
+  notify-send -u low -t 1000 -h int:value:$volume "Volume: $volume%" "(UNMUTED)"
 fi
