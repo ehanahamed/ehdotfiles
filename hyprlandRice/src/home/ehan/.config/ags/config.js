24b645b7c4813dfb969c1c21355193937815c7a2
Ehan Ahamed <ehanahamed@ehan.dev>
2023-12-17 12:56:49 -0500

    wow

--- hyprlandRice/src/home/ehan/.config/ags/config.js
+++ hyprlandRice/src/home/ehan/.config/ags/config.js
@@ -1,42 +1,38 @@
-import TopBar from './js/bar/TopBar.js';
-import ScreenCorners from './js/screencorner/ScreenCorners.js';
-import Overview from './js/overview/Overview.js';
-import Dashboard from './js/dashboard/Dashboard.js';
-import OSD from './js/osd/OSD.js';
-import Applauncher from './js/applauncher/Applauncher.js';
-import PowerMenu from './js/powermenu/PowerMenu.js';
-import Verification from './js/powermenu/Verification.js';
-import Desktop from './js/desktop/Desktop.js';
-import Notifications from './js/notifications/Notifications.js';
-import QuickSettings from './js/quicksettings/QuickSettings.js';
-import Lockscreen from './js/lockscreen/Lockscreen.js';
-import options from './js/options.js';
-import * as setup from './js/utils.js';
-import { forMonitors } from './js/utils.js';
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+import Hyprland from "resource:///com/github/Aylur/ags/service/hyprland.js";
 
-setup.warnOnLowBattery();
-setup.scssWatcher();
-setup.globalServices();
+function Bar(monitor) {
+    function label(monitor) {
+        const labelLabel = Widget.Label({
+            label: "this is monitor " + monitor,
+        })
 
-export default {
-    maxStreamVolume: 1.5,
-    cacheNotificationActions: true,
-    closeWindowDelay: {
-        'quicksettings': options.windowAnimationDuration,
-        'dashboard': options.windowAnimationDuration,
-    },
-    windows: [
-        forMonitors(TopBar),
-        forMonitors(ScreenCorners),
-        forMonitors(OSD),
-        forMonitors(Desktop),
-        forMonitors(Notifications),
-        forMonitors(Lockscreen),
-        Applauncher(),
-        Overview(),
-        Dashboard(),
-        QuickSettings(),
-        PowerMenu(),
-        Verification(),
-    ].flat(2),
-};
+        return labelLabel;
+    }
+    
+    const window = Widget.Window({
+        monitor,
+        // name has to be unique
+        name: `bar${monitor}`,
+        anchor: ['top', 'left', 'right'],
+        exclusivity: "exclusive",
+        child: label(monitor),
+    })
+
+    return window
+}
+
+/* barList starts with just one bar on monitor 0 */
+var barList = [Bar(0)];
+
+/* Then, we use Hyprland.monitors to add more bars to barList */
+for (
+    i = 1; /* for loop starts at 1, because monitor 0 is already added */
+    i < Hyprland.monitors.length;
+    i++
+) {
+    barList.push(Bar(i));
+}
+
+/* Now, we set windows to barList, so that all the bars are shown */
+export default { windows: barList }
