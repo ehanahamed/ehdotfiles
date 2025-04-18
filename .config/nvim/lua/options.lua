7e43dd497680ed16231b8800db0b81e22ff222d0
Ehan <ehanahamed@ehan.dev>
2025-04-18 02:04:07 -0400

    nvm

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,15 +3,3 @@ require "nvchad.options"
 -- add yours here!
 local o = vim.o
 o.cursorlineopt = 'both' -- to enable cursorline!
-
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("VimEnter", {
-  -- `--password` has limited permissions, this is NOT a "real" password/secret
-  command = ":silent !kitty @ --password=\"estrogen\" set-spacing padding=0 margin=0",
-})
-
-autocmd("VimLeavePre", {
-  -- `--password` has limited permissions, this is NOT a "real" password/secret
-  command = ":silent !kitty @ --password=\"estrogen\" set-spacing padding=20 margin=10",
-})
