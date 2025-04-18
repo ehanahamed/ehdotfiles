fa41d22615ea9820f1da40da370164528c9380bf
Ehan <ehanahamed@ehan.dev>
2025-04-18 14:17:31 -0400

    plugins/init.lua

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "psliwka/vim-smoothie",
+    lazy = false
+  }
+  -- These are some examples, uncomment them if you want to see them work!
+  
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
