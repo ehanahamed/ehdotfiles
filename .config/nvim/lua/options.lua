1d6556657b5ffb40d694f4fbc1f7f7b615c9ed87
Ehan <ehanahamed@ehan.dev>
2025-04-18 01:54:01 -0400

    asfd

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,11 +7,11 @@ o.cursorlineopt = 'both' -- to enable cursorline!
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("VimEnter", {
-  -- `--password` has scoped permissions, this is a NOT-very-secret password
+  -- `--password` has limited permissions, this is NOT a "real" password/secret
   command = ":silent !kitty @ --password=\"estrogen\" set-spacing padding=0 margin=0",
 })
 
 autocmd("VimLeavePre", {
-  -- `--password` has scoped permissions, this is a NOT-very-secret password
+  -- `--password` has limited permissions, this is NOT a "real" password/secret
   command = ":silent !kitty @ --password=\"estrogen\" set-spacing padding=20 margin=10",
 })
