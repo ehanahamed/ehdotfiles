57ec548124cfaa7ab3fc831de42f5a88b8c2d838
ehanahamed <ehanahamed@ehan.dev>
2025-04-22 20:57:14 +0000

    Add .config/ags/user_options.js

---
+++ .config/ags/user_options.js
@@ -0,0 +1,22 @@
+// For every option, see ~/.config/ags/modules/.configuration/user_options.js
+// (vscode users ctrl+click this: file://./modules/.configuration/user_options.js)
+// (vim users: `:vsp` to split window, move cursor to this path, press `gf`. `Ctrl-w` twice to switch between)
+//   options listed in this file will override the default ones in the above file
+
+const userConfigOptions = {
+    "apps": {
+        "terminal": "kitty"
+    },
+    'time': {
+        // See https://docs.gtk.org/glib/method.DateTime.format.html
+        // Here's the 12h format: "%I:%M%P"
+        // For seconds, add "%S" and set interval to 1000
+        'format': "%H:%M:%S",
+        'interval': 1000,
+        'dateFormatLong': "Y-%m-%d %b WV-%u %a", // On bar
+        'dateInterval': 5000,
+        'dateFormat': "Y-%m-%d WV-%u %a", // On notif time
+    },
+}
+
+export default userConfigOptions;
