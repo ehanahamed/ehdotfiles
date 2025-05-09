5cd33c319eac629aab1b4418a0c8f9b879a7c495
Ehan <ehanahamed@ehan.dev>
2025-05-09 17:58:00 -0400

    Update init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,6 +4,7 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
+
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -11,13 +12,25 @@ return {
     end,
   },
   {
-    "psliwka/vim-smoothie",
+    "tpope/vim-repeat",
+    lazy = false
+  },
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function ()
+      require("leap").add_default_mappings()
+    end
+  },
+  {
+    "tpope/vim-surround",
     lazy = false
   }
   -- These are some examples, uncomment them if you want to see them work!
-  
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
