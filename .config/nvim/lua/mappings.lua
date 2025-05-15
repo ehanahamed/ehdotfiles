668fd7bded5d9dbc812e63ba6fbcad39073caf2a
ehanahamed <ehanahamed@ehan.dev>
2025-05-15 01:02:30 +0000

    Update .config/nvim/lua/mappings.lua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,7 +4,6 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 map("t", "<ESC>", "<C-\\><C-n>", { desc = "ESC terminal normal mode", noremap = true })
 
