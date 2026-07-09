54f64f146c17b7c962db607565d1042124ba03fa
Ehan A <ehanahamed@ehan.dev>
2026-07-09 00:43:25 -0400

    Update general.lua

--- .config/hypr/custom/general.lua
+++ .config/hypr/custom/general.lua
@@ -1,7 +1,7 @@
 hl.config({
     input = {
-        kb_layout = "us,us",
-        kb_variant = "altgr-intl,rus",
+        kb_layout = "us,us,gr",
+        kb_variant = "altgr-intl,rus,polytonic",
         kb_options = "grp:ctrl_space_toggle, compose:rctrl, caps:escape, shift:both_capslock"
     }
 })
